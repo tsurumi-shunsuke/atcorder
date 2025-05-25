56ddbfc0274f5087c312381672571eca2473d158
tsurumi <2rumi1127@gmail.com>
2025-05-25 14:30:36 +0900

    ABC407　Cまで

--- ABC407/C.cpp
+++ ABC407/C.cpp
@@ -8,11 +8,24 @@ using namespace std;
 int main () {
     string S;
     cin >> S;
-    int count = 0;
+    int answer = 0;
 
+    for (int i = 0; i < S.size(); i++) {
+        int A_button = 1;
+        int B_button = 0;
+        int num = S[i] - '0';
 
+        if (i != S.size() - 1) {
+            int next_num = S[i + 1] - '0';
+            B_button = (num - next_num + 10) % 10;
+        } else {
+            B_button = num;
+        }
 
+        answer += (B_button + A_button);
+    }
 
+    cout << answer << endl;
 
     return 0;
 }
