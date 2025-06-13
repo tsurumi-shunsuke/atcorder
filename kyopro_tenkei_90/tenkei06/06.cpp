ebe5b846fe986795ab82fe2501b14908cbe0275b
tsurumi <2rumi1127@gmail.com>
2025-06-13 12:20:51 +0900

    典型問題06完了

--- kyopro_tenkei_90/tenkei06/06.cpp
+++ kyopro_tenkei_90/tenkei06/06.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main () {
@@ -7,5 +8,22 @@ int main () {
     string S;
     cin >> N >> K >> S;
 
+    vector<char> ans;
+    int head = 0;
+
+    for (int i = 0; i < K; i++) {
+        int min_pos = head;
+        for (int j = head; j <= N - (K - i); j++) {
+            if (S[min_pos] > S[j]) {
+                min_pos = j;
+            }
+        }
+        ans.push_back(S[min_pos]);
+        head = min_pos + 1;
+    }
     
+    for (char c : ans) cout << c;
+    cout << endl;
+
+    return 0;
 }
